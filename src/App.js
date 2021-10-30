9202cb23e73e2550c40a1d6deb853a2a7d42e07b
201930322LeeJunSun <wnstjsektzja@naver.com>
2021-10-30 19:55:11 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,57 +1,18 @@
-import { render } from "@testing-library/react";
 import React from "react";
-import axios from "axios";
-import Movie from './Movie';
 import './App.css'
+import {HashRouter, Route} from 'react-router-dom';
+import About from './routes/About'
+import Home from "./routes/Home";
+import Navigation from "./components/Navigation";
 
-class App extends React.Component{
-  state = {
-    isLoding : true,
-    movies:[]
-  };
-
-  getMovies = async () => {
-    const{
-      data: {
-        data: {movies},
-      },
-    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
-    this.setState({movies, isLoding:false});
-  }
-
-  componentDidMount(){
-
-    this.getMovies();
-  }
-  render(){
-    const {isLoding, movies }= this.state;
-    return (
-    <section className ="container">
-      {isLoding ?(
-      <div className ="loder">
-        <span className="loder__text">Loding...</span>
-        </div>
-    ) : (
-      <div className ="movies">
-       {movies.map(movie=>(
-      <Movie
-      key={movie.id}
-      id={movie.id}
-      year={movie.year}
-      title={movie.title}
-      summary={movie.summary}
-      poster={movie.medium_cover_image}
-      genres={movie.genres}
-      />
-       ))}
-       </div>
-    )}
-    </section>
-    )
-  }
-
-  }
-  
-  
+function App() {
+  return(
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      </HashRouter>
+  );
+}
 
 export default App;
